8e9d86b787356c70d5fee10e008ad46039dda7bd
imnahidul <withnahidulislam@gmail.com>
2025-12-28 09:10:52 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+// import express from 'express';
+// import cors from 'cors';
+// import connectDB from './db/connection.js';
+// import authRoutes from './routes/auth.js';
+// //import categoryRoutes from '../routes/category.js';
+// //import categoryRoutes from '../server/routes/category.js'
+// import categoryRoutes from './routes/category.js';
+// import SupplierRoutes from './routes/supplier.js';
+// import productRoutes from './routes/product.js';
+// import userRoutes from './routes/user.js';
+// import orderRoutes from './routes/order.js';
+// //import dashboardRouters from './routers/dashboard.js';
+// import dashboardRoutes from './routes/dashboard.js'
+// import invoiceRoutes from './routes/invoice.js'
+// import estimateRoutes from "./routes/estimate.js";
+// import requisitionRoutes from "./routes/requisition.js";
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api/auth',authRoutes);
+// //app.use('/api/category',categoryRoutes);
+// app.use('/api/category',categoryRoutes);
+// app.use('/api/supplier',SupplierRoutes);
+// app.use('/api/products',productRoutes);
+// app.use('/api/users',userRoutes);
+// app.use('/api/orders',orderRoutes);
+// //app.use('/api/dashboard',dashboardRouters);
+// app.use('/api/dashboard',dashboardRoutes);
+// app.use('/api/invoice',invoiceRoutes);
+// app.use("/api/estimate", estimateRoutes);
+// app.use("/api/requisition", requisitionRoutes);
+
+// app.listen(process.env.PORT, () =>{
+// connectDB();
+//     console.log('server is running on http://localhost:3000');
+// }
+// )
+
+
+
+///////after connect with mongo atlas 6,7,8,9-12-25--Running---/////////////////
+
+import dotenv from 'dotenv';
+dotenv.config();                              //  
+
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db/connection.js';
+import authRoutes from './routes/auth.js';
+import categoryRoutes from './routes/category.js';
+import SupplierRoutes from './routes/supplier.js';
+import productRoutes from './routes/product.js';
+import userRoutes from './routes/user.js';
+import orderRoutes from './routes/order.js';
+import dashboardRoutes from './routes/dashboard.js';
+import invoiceRoutes from './routes/invoice.js';
+import estimateRoutes from "./routes/estimate.js";
+import requisitionRoutes from "./routes/requisition.js";
+
+const app = express();
+/////////app.use(cors({ origin: "https://vision-project-3ntg.vercel.app", credentials: true }));
+//app.use(cors());
+ app.use(cors({
+  origin: "http://localhost:5173", // your frontend origin
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"], // important!
+}));
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/supplier', SupplierRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/dashboard', dashboardRoutes);
+app.use('/api/invoice', invoiceRoutes);
+app.use("/api/estimate", estimateRoutes);
+app.use("/api/requisition", requisitionRoutes);
+
+// Fixed port + DB connection
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, async () => {
+  await connectDB();                          
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log('MongoDB  Connected!');
+});
+
+
---
+++ models/Product.js
@@ -0,0 +1,40 @@
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema ({
+//     name :{type: String, required: true},
+//     description :{type: String, required: true},
+//     price :{type: Number, required: true},
+//     stock :{type: Number, required: true},
+//     isDeleted:{type: Boolean, default: false},
+//     categoryId : {type: mongoose.Schema.Types.ObjectId, ref:"Category", required: true},
+//     supplierId: {type: mongoose.Schema.Types.ObjectId, ref:"Supplier", required: true},
+
+//     });
+
+// const ProductModel = mongoose.model("Product", productSchema);
+
+// export default ProductModel;    
+
+
+/////////////////////products date add 18-12-2025////////////////////////////
+
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema ({
+    name :{type: String, required: true},
+    description :{type: String, required: true},
+    price :{type: Number, required: true},
+    stock :{type: Number, required: true},
+   // stockDate: { type: Date,default: Date.now, },
+   stockDate: {type: String,required: true,
+  //default: () => new Date().toISOString().split('T')[0]  
+},
+    isDeleted:{type: Boolean, default: false},
+    categoryId : {type: mongoose.Schema.Types.ObjectId, ref:"Category", required: true},
+    supplierId: {type: mongoose.Schema.Types.ObjectId, ref:"Supplier", required: true},
+
+    });
+
+const ProductModel = mongoose.model("Product", productSchema);
+
+export default ProductModel; 
